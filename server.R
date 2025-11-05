7d7298d7b950ad601ddce7e67794eb14444ac2af
Edene Levine <s240875@dtu.dk>
2025-11-05 10:56:28 +0000

    new update for DNA

--- server.R
+++ server.R
@@ -1,4 +1,8 @@
 server <- function(input, output) {
+  output$dna <- renderText({
+    gene_dna(lemgth = input$n_bases,
+             base_probs = c(input$prob_A, input$prob_T, input$prob_C, input$prob_G))
+  })
   rna_seq <- reactive({
     transcribe_dna(dna_seq())
   })
